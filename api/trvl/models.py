b3f31a6d966c5ce66865f2b60e1d8ee0406bbc7d
leoMurtha <aka.murtha@gmail.com>
2019-03-07 19:10:38 +0100

    created models relational

---
+++ api/trvl/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Airport(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.TextField()
+
+class Carrier(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.TextField()
+
+class FlightStatistics(models.Model):
+    cancelled = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    on_time = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    total = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    delayed = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    diverted = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+
+class DelayCountStatistics(models.Model):
+    late_aircraft = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    weather = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    security = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    national_aviation_system = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    carrier = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+
+class DelayTimeStatistics(models.Model):
+    late_aircraft = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    weather = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    security = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    national_aviation_system = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+    carrier = models.IntegerField(validators=[MinValueValidator(0, message='Invalid value: negative.')], default=0)
+
+class Statistics(models.Model):
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    carrier = models.ForeignKey(Carrier, on_delete=models.CASCADE)
+    month = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
+    year = models.IntegerField(validators=[MinValueValidator(1900, message='Invalid year: year < 1900.')])
+    flight_statistic = models.ForeignKey(FlightStatistics, on_delete=models.DO_NOTHING)
+    delay_count_statistic = models.ForeignKey(DelayCountStatistics, on_delete=models.DO_NOTHING)
+    delay_time_statistic = models.ForeignKey(DelayTimeStatistics, on_delete=models.DO_NOTHING)
+    
+    # Guaranteeing the "primary key" of the tuple
+    class Meta:    
+        unique_together = ('airport', 'carrier', 'month', 'year')
