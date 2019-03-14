beeb4317e3ad9e85cec0a607f61c7e2f34512f0e
leoMurtha <aka.murtha@gmail.com>
2019-03-14 19:25:06 +0100

    feeding

--- feed.py
+++ feed.py
@@ -7,10 +7,20 @@ import requests
 def feed_api(entries, keys):
 
     for entry in entries:
-        for key in keys:
-            print('key %s data %s' % (key, entry[key]))
-            #requests.post('localhost:8000/%s',)
-        return 0
+        #print('key %s data %s' % (key, entry[key]))
+        
+        requests.post(url='http://127.0.0.1:8000/api/carriers/', data=entry['carrier'])
+        
+        data = entry['airport']
+
+        data.update({'carriers': ['http://127.0.0.1:8000/api/carriers/%s/' % entry['carrier']['code']]})
+
+        print(data)
+        try:
+            requests.post('http://localhost:8000/api/airports/', data=data)
+        except:
+            requests.patch('http://localhost:8000/api/airports/%s' % data['airport']['code'], data=data)
+        #return 0
 
 if __name__ == "__main__":
     # Reading and doing some processing
@@ -20,4 +30,4 @@ if __name__ == "__main__":
           (len(entries), keys))
     
     # Feed to the db
-    feed_api(entries, ['airport', 'carrier'])
+    feed_api(entries, ['airport'])
