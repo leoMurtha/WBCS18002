3c508eb6bad1ed0d9b76171ce539665b779a1a14
leoMurtha <aka.murtha@gmail.com>
2019-03-17 20:29:55 +0100

    no need for url formats import

--- api/trvl/urls.py
+++ api/trvl/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import url
 
 router = routers.DefaultRouter()
@@ -14,7 +13,3 @@ router.register('statistics', views.StatisticsView)
 urlpatterns = [
     path('', include(router.urls))
 ]
-
-print(router.urls)
-
-#urlpatterns = format_suffix_patterns(urlpatterns)
