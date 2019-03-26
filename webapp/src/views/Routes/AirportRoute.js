054af1a8c051103bc46413bf1c70b03bbcaeee2c
leoMurtha <aka.murtha@gmail.com>
2019-03-26 22:19:10 +0100

    cors

--- webapp/src/views/Routes/AirportRoute.js
+++ webapp/src/views/Routes/AirportRoute.js
@@ -22,9 +22,6 @@ class AirportRoute extends Component {
     axios.get(url, {
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
       }
       })
       .catch((err, response) => {
