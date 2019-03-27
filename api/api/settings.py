109bfce63865179ca7b12c0d9ada569a0fb18689
leoMurtha <aka.murtha@gmail.com>
2019-03-27 15:25:52 +0100

    cors

--- api/api/settings.py
+++ api/api/settings.py
@@ -52,10 +52,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
-
-APPEND_SLASH=False
 
 CORS_ORIGIN_ALLOW_ALL = True
 
