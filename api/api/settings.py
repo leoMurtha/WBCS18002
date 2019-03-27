b68c110393b58106924db5882a244d862cd5ffff
leoMurtha <aka.murtha@gmail.com>
2019-03-27 16:32:46 +0100

    vou sair hoje

--- api/api/settings.py
+++ api/api/settings.py
@@ -54,13 +54,8 @@ MIDDLEWARE = [
     #'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
-    '127.0.0.1:9000'
-)
-
-
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'api.urls'
 
