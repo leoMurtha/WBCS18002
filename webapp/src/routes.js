1bb47572badafebcf7dc37867584ac7251699676
leoMurtha <aka.murtha@gmail.com>
2019-03-27 15:36:15 +0100

    Merge branch 'master' of github.com:leoMurtha/WBCS18002

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -13,6 +13,7 @@ const routes = [
   { path: '/', exact: true, name: 'Home' },
 
   { path: '/routes', name: 'Routes', component: Routes },
+
   { path: '/airports/:id/routes', exact:true , name: 'Routes', component: Routes },
   { path: '/airports/:id/routes/:destination', name: 'AirportRoute', component: AirportRoute },
   { path: '/carriers', exact: true, name: 'Carriers', component: Carriers  },
