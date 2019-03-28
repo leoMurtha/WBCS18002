93cac44aa534c1a0940e4d697bcc4b26af885746
leoMurtha <aka.murtha@gmail.com>
2019-03-28 14:07:26 +0100

    flights

---
+++ webapp/src/views/Statistics/Flights.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { ListGroup, ListGroupItem, Button, ButtonGroup } from 'reactstrap';
+import axios from 'axios';
+
+class Flights extends Component {
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      carriers: [],
+    }
+  }
+  componentWillMount() {
+    this._isMounted = false;
+    axios.defaults.baseURL = 'http://trvl.hopto.org:8000/api/';
+
+  }
+  componentDidMount() {
+    this._isMounted = true;
+
+    let url = `carriers/`;
+
+    axios.get(url)
+      .then(res => {
+        if (this._isMounted) {
+          this.setState({'carriers': res.data});
+
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    console.log(this.props.match);
+    return (
+      <div className="animated fadeIn">
+        <ListGroup flush>
+          {this.state.carriers.map((carrier) => {
+            return (
+              <ListGroupItem key={carrier.code} tag='button' action>
+                <Button outline color="primary" tag={Link} to={`/carriers/${carrier.code}`}>{carrier.code}</Button>
+              </ListGroupItem>
+            )
+          })}
+        </ListGroup>
+      </div>
+    );
+  }
+}
+
+export default Flights;
