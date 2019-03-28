70178e7126f928a7c0defa444a8a39cf7a534e1e
VitorBrisola <vitoraugusto199@gmail.com>
2019-03-28 13:51:05 +0100

    dropdown forms

--- webapp/src/views/Calendar/SelectDate.js
+++ webapp/src/views/Calendar/SelectDate.js
@@ -13,24 +13,25 @@ import {
 
 //import Select from 'react-select';
 
-const canlendar_months = {
-  January: "Jan",
-  February: "Feb",
-  March: "Mar",
-  April: "Apr",
-  May: "May",
-  June: "Jun",
-  July: "Jul",
-  August: "Aug",
-  September: "Sep",
-  October: "Oct",
-  November: "Nov",
-  December: "Dec"
-}
+var canlendar_months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+]
 
-var years = [2000,2001,2002,2003]
+var years = ["2000","2001","2002","2003"]
 
 class SelectDate extends Component {
+  
   
     
   constructor(props){
@@ -67,65 +68,59 @@ class SelectDate extends Component {
         }
       });
     
+    console.log(this.state);
   }
 
-  triggerDropdown(){
-
-    var already = {};
-    var sel = document.getElementById('years_listing');
-
-    for (var i = 0; i < years.length; i++) 
-    {
-      var val = years[i];
-
-      if (! already[val]) 
-      {
-        var opt = document.createElement('option');
-        opt.innerHTML = val;
-        opt.value = val;
-        sel.appendChild(opt);
-
-        already[val] = true;
-      }
-    }
-};
+//  triggerDropdown(){
+//
+//    var already = {};
+//    var sel = document.getElementById('years_listing');
+//
+//    for (var i = 0; i < years.length; i++) 
+//    {
+//      var val = years[i];
+//
+//      if (! already[val]) 
+//      {
+//        var opt = document.createElement('option');
+//        opt.innerHTML = val;
+//        opt.value = val;
+//        sel.appendChild(opt);
+//
+//        already[val] = true;
+//      }
+//    }
+//};
   
   
-   render() {
+render() {
 
       return (
-            <div className="airports">
-            <Card>
-              <CardHeader>
-                <strong>Credit Card</strong>
-                <small> Form</small>
-              </CardHeader>
-              <CardBody>
-                <Row>
-                  <Col xs="4">
-                    <FormGroup>
-                      <Label htmlFor="ccmonth">Month</Label>
+        <div className="airports">
+          <Card>
+            <CardHeader>
+              <strong>Fred Card</strong>
+              <small> Form</small>
+            </CardHeader>
+          <CardBody>
+            <Row>
+              <Col xs="4">
+                <FormGroup>
+                  <Label htmlFor="ccmonth">Month</Label>
                       <Input type="select" name="ccmonth" id="ccmonth">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
+                        {canlendar_months.map(canlendar_months => (
+                            <option>{canlendar_months}</option>
+                        ))}
                       </Input>
                     </FormGroup>
                   </Col>
                   <Col xs="4">
                     <FormGroup>
                       <Label htmlFor="ccyear">Year</Label>
-                      <Input type="select" name="ccyear" id="year">
-                        <select id="years"></select>
+                      <Input type="select" name="ccyear" id="ccyear">
+                        {years.map(years => (
+                            <option>{years}</option>
+                        ))}
                       </Input>
                     </FormGroup>
                   </Col>
