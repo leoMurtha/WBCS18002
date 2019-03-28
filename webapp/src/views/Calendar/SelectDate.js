986e70dd696fd91fb94fea81d3095b0981050673
VitorBrisola <vitoraugusto199@gmail.com>
2019-03-28 18:13:12 +0100

    select date fix

--- webapp/src/views/Calendar/SelectDate.js
+++ webapp/src/views/Calendar/SelectDate.js
@@ -28,19 +28,18 @@ var canlendar_months = [
   "Dec"
 ]
 
-var years = ["2000","2001","2002","2003"]
+var years = ["2000", "2001", "2002", "2003"]
 
 class SelectDate extends Component {
-  
-  
-    
-  constructor(props){
+
+
+  constructor(props) {
     super(props);
 
     this.state = {
       url_departure: null,
       url_destination: null,
-      carrier: {},   
+      carrier: {},
       minimal_statistics: [],
       months: [],
       years: [],
@@ -57,7 +56,7 @@ class SelectDate extends Component {
 
     let url_departure = `carriers/${this.props.match.params.carrier}/statistics?type=minimal&aiport=${this.props.match.params.id}`;
     //let url_destination = `carriers/${this.props.match.params.carrier}/statistics?type=minimal&aiport=${this.props.match.params.destination}`;
-    
+
     axios.get(url_departure)
       .catch((err) => {
         console.error(err);
@@ -66,84 +65,89 @@ class SelectDate extends Component {
         if (this._isMounted) {
           this.setState(res.data);
 
+          var months = []
+          var years = []
           res.data.minimal_statistics.forEach(statistic => {
-              //if(! this.state.months.includes(statistic.date.month)){ 
-              //  this.state.months.push(statistic.date.month)
-              //} 
-              if(! this.state.years.includes(statistic.date.year)){ 
-                this.state.years.push(statistic.date.year)
-              } 
+            if (!months.includes(statistic.date.month)) {
+              months.push(statistic.date.month)
+            }
+            if (!years.includes(statistic.date.year)) {
+              years.push(statistic.date.year)
+            }
           });
-
-          //console.log(this.state.months);
-          console.log(this.state.years);
+          this.setState({months: months.sort()})
+          this.setState({years: years.sort()})
+          
+          console.warn(this.state.months);
+          console.warn(this.state.years);
         }
       });
-    
+
     //console.log(this.state);
   }
 
 
-//  triggerDropdown(){
-//
-//    var already = {};
-//    var sel = document.getElementById('years_listing');
-//
-//    for (var i = 0; i < years.length; i++) 
-//    {
-//      var val = years[i];
-//
-//      if (! already[val]) 
-//      {
-//        var opt = document.createElement('option');
-//        opt.innerHTML = val;
-//        opt.value = val;
-//        sel.appendChild(opt);
-//
-//        already[val] = true;
-//      }
-//    }
-//};
-  
-  
-render() {
+  //  triggerDropdown(){
+  //
+  //    var already = {};
+  //    var sel = document.getElementById('years_listing');
+  //
+  //    for (var i = 0; i < years.length; i++) 
+  //    {
+  //      var val = years[i];
+  //
+  //      if (! already[val]) 
+  //      {
+  //        var opt = document.createElement('option');
+  //        opt.innerHTML = val;
+  //        opt.value = val;
+  //        sel.appendChild(opt);
+  //
+  //        already[val] = true;
+  //      }
+  //    }
+  //};
 
-      return (
-        <div className="airports">
-          <Card>
-            <CardHeader>
-              <strong>Fred Card</strong>
-              <small> Form</small>
-            </CardHeader>
+
+  render() {
+    console.log(this.state.years);
+
+    return (
+      <div className="airports">
+        <Card>
+          <CardHeader>
+            <strong>Fred Card</strong>
+            <small> Form</small>
+          </CardHeader>
           <CardBody>
             <Row>
               <Col xs="4">
                 <FormGroup>
                   <Label htmlFor="ccmonth">Month</Label>
-                      <Input type="select" name="ccmonth" id="ccmonth">
-                        {this.state.months.map(months => (
-                            <option>{months}</option>
-                        ))}
-                      </Input>
-                    </FormGroup>
-                  </Col>
-                  <Col xs="4">
-                    <FormGroup>
-                      <Label htmlFor="ccyear">Year</Label>
-                      <Input type="select" name="ccyear" id="ccyear">
-                        {this.state.years.map(years => (
-                            <option>{years}</option>
-                        ))}
-                      </Input>
-                    </FormGroup>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-        </div>
-      );
-    }
+                  <Input type="select" name="ccmonth" id="ccmonth">
+                    {console.log(this.state.months)}
+                    {this.state.months.map(month => (
+                      <option>{month}</option>
+                    ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col xs="4">
+                <FormGroup>
+                  <Label htmlFor="ccyear">Year</Label>
+                  <Input type="select" name="ccyear" id="ccyear">
+                    {this.state.years.map(year => (
+                      <option>{year}</option>
+                    ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      </div>
+    );
   }
-  
-  export default SelectDate;
-  
+}
+
+export default SelectDate;
